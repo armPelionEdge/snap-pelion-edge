2a6446a297c211f0d4ef805539e03de72bc825f5
Michael Ray <michael.ray@arm.com>
2021-07-07 17:08:54 -0500

    Copy fluentbit config to snap_data, change log level to warn

--- files/fluent-bit/scripts/launch-fluent-bit.sh
+++ files/fluent-bit/scripts/launch-fluent-bit.sh
@@ -5,4 +5,8 @@ if [ ! -f ${SNAP_DATA}/userdata/edge_gw_identity/identity.json ]; then
     exit 1
 fi
 
+if [ ! -f "$SNAP_DATA/fluent-bit.conf" ]; then
+    cp "$SNAP/fluent-bit.conf" "$SNAP_DATA/fluent-bit.conf"
+fi
+
 exec ${SNAP}/wigwag/system/bin/fluent-bit -c $SNAP_DATA/fluent-bit.conf
